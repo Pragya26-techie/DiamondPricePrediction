3a804ca2560392591b518c0c44d9e2148ed7c000
Pragya26-techie <pragyarajput2662@gmail.com>
2025-05-16 13:51:05 +0530

    Model Trained

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import pickle
 import numpy as np
 import pandas as pd
+from sklearn.metrics import r2_score
 
 
 from src.exception import CustomException
@@ -19,3 +20,25 @@ def save_object(file_path,obj):
     
     except Exception as e:
         raise CustomException(e,sys)
+    
+def evaluate_model(x_train,y_train,x_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            # Train model
+            model.fit(x_train,y_train)
+
+            # Predict testing data
+            y_test_pred = model.predict(x_test)
+
+            # Get R2 score for train and test data
+            test_model_score = r2_score(y_test,y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    except Exception as e:
+        logging.info("Error occured during model training")
+        raise CustomException(e,sys)
+
+
